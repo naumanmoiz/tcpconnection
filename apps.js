3025d31a1d494826e7c45d8982dc7763e0276a84
ceet <ceet@192.168.0.110>
2017-12-19 14:46:31 -0600

    inze commit

--- apps.js
+++ apps.js
@@ -1,22 +1,72 @@
 var net = require('net');
+//
+//var server = net.createServer(function(socket) {
+//	socket.write('Echo server\r\n');
+//	socket.pipe(socket);
+//});
+//
+//server.listen(1338, '127.0.0.1');
+//
+//
 
-var server = net.createServer(function(socket) {
-	socket.on('data', function(data){
-		var response = data.toString().trim();
-		console.log(response + '\r\n');
-	});
-});
 
-server.listen(1337, '127.0.0.1');
 
 var client = new net.Socket();
-client.connect(1338, '127.0.0.1', function() {
-	console.log('Connected');
+var HOST = '127.0.0.1'; var PORT = '1338';
+client.connect(PORT, HOST, function() {
+	console.log('Connected' + HOST + PORT);
 	client.write('Hello, server! Love, Client.');
-	console.log('Hello, server! Love, Client.');
+	client.push('Hello,world');
+	//client.write('Hi');
 });
+
 
 client.on('data', function(data) {
 	console.log('Received: ' + data);
 	client.destroy(); // kill client after server's response
 });
+
+
+client.on('error', function(e) {
+
+    while (e.code == 'ECONNREFUSED') {
+        console.log('Is the server running at ' + PORT + '?');
+
+        client.setTimeout(4000, function() {
+
+            client.connect(PORT, HOST, function() {
+                console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+                client.write('I am inner Superman');
+            });         
+
+            console.log('Timeout for 5 seconds before trying port:' + PORT + ' again');
+        });
+    }
+});
+
+//client.on('data', function(data) {
+//    console.log('DATA: ' + data);
+//    client.destroy();
+//});
+
+client.on('close', function() {
+    console.log('Connection closed');
+});
+
+//Add a 'close' event handler for the client socket
+//client.on('close', function() {
+//    console.log('Connection closed');
+//});
+
+
+//client.on('error', function(err) {
+//	   console.log(err)
+//	});
+////client.forEach(function(client) {
+////    client.destroy();
+////});
+//client.on('close', function() {
+//	console.log('Connection closed');
+//});
+
+//server.close();
