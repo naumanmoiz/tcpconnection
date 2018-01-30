13c401dae48d9fce9dd843dc9447ea2e86c13efe
Nauman Moiz Mohammed Abdul <naumanmoiz@gmail.com>
2018-01-30 14:40:38 -0600

    Updated tcp new file

--- tcpnew.js
+++ tcpnew.js
@@ -43,11 +43,18 @@ client.connect(6801, '127.0.0.1', function() {
 });
 
 
-client.on('data', function(data) {
+client.on('data', data);
+
+function data (data) {
 	console.log('Received: ' + data);
 	//client.destroy(); // kill client after server's response
-});
+};
+var http = require('http');
 
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(data);
+}).listen(8080);
 
 client.on('close', function() {
 	console.log('Connection closed');
