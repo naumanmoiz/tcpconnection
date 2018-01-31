934d9008324a5bb1219b02ecb0583bae6a14eeda
naumanmoiz <naumanmoiz@gmail.com>
2018-01-31 10:20:05 -0600

    m

--- tcpnew.js
+++ tcpnew.js
@@ -33,7 +33,7 @@ http://www.hacksparrow.com/tcp-socket-programming-in-node-js.html.) */
 var net = require('net');
 
 var client = new net.Socket();
-client.connect(6801, '131.156.156.32', function() {
+client.connect(6801, '127.0.0.1', function() {
 	console.log('Connected');
 	
 		client.write('Hello, server!! Love, Client.');
